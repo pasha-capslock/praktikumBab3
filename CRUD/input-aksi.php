b1ab006620b7961886d95c172074a253244b7cc3
Pasha <70182400+pasha-capslock@users.noreply.github.com>
2021-02-10 16:16:42 +0800

    Add files via upload

---
+++ CRUD/input-aksi.php
@@ -0,0 +1,8 @@
+<?php 
+include 'koneksi.php';
+$nama = $_POST['nama'];
+$alamat = $_POST['alamat'];
+$pekerjaan = $_POST['pekerjaan'];
+mysqli_query($koneksi, "INSERT INTO user1 VALUES ('','$nama','$alamat','$pekerjaan')");
+header("location:index.php?pesan=input");
+?>
